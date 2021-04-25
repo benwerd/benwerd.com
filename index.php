6e31466972b060c034aaf84fb7fbe5dff4612d73
Ben Werdmuller <benwerdmuller@werd.lan>
2021-04-24 18:07:27 -0700

    Metatags and headers

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+    header('x-clacks-overhead: Terry Pratchett (but no longer GNU)');
+
+?>
+<!doctype html>
+
+<!-- HTML is a programming language -->
+<html lang="en-US" dir="ltr">
+    <head>
+
+        <!-- Look! It's my name! -->
+        <title>
+            Ben Werdmüller
+        </title>
+
+        <!-- If I don't include this, your browser is going to do something funky with that umlaut -->
+        <meta charset="utf-8">
+
+        <!-- I hate writing descriptions of myself ... if you can think of a better one, hit me up: ben@benwerd.com -->
+        <meta name="description" content="Ben Werdmüller is an engineer, founder, investor, and writer. He works on ethical tech.">
+
+        <!-- I wrote this HTML code with my own two fingers -->
+        <meta name="generator" content="Handmade">
+
+        <!-- Unfortunately, I wrote this CSS, too -->
+        <link rel="stylesheet" href="/css/main.css">
+
+        <!-- Credit for this emoji favicon hack goes to Lea Verou: https://twitter.com/leaverou/status/1241619866475474946?lang=en -->
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎉</text></svg>">
+
+        <!-- I included this specially for your 2008 iPod Touch. You're welcome -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <!-- To silo is human; to syndicate, divine -->
+        <link rel="alternate" type="application/rss+xml" title="Ben Werdmüller" href="https://werd.io/?_t=rss"/>
+
+        <!-- Look, man, we all gotta make a buck -->
+        <meta name="monetization" content="$ilp.uphold.com/7yhraRBZn2GY">
+
+        <!-- I am spying on you. Sorry / not sorry -->
+        <script type="text/javascript">
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+                var u="https://werd.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '2']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/werd.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+        </script>
+
+    </head>
+    <body>
+        <h1>
+            Hello, I'm Ben Werdmüller
+        </h1>
+    </body>
+</html>
