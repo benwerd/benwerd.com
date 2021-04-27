e518c680a2d11f43a5ad69422533e7b3f10a4081
Ben Werdmuller <ben@benwerd.com>
2021-04-26 18:07:29 -0700

    Further language tweak; minor CSS change

--- index.php
+++ index.php
@@ -103,15 +103,14 @@
                 of startups (as well as starting a few of my own).
             </p>
             <p>
-                I'd love to be helpful if I can, and can advise on a retainer or equity basis.
+                I'd love to be helpful if I can.
                 <a href="mailto:ben@benwerd.com">Get in touch at ben@benwerd.com</a>.
             </p>
             <p>
                 I believe in inclusivity, building real equity, and subverting systemic oppression by giving
-                a voice and a platform to diverse peoples. I will not knowingly work on projects that cause harm
-                to marginalized communities or persist existing inequalities, nor with project founders who do not
-                agree with those goals. Because I care deeply about helping build a more inclusive ecosystem,
-                I advise early-stage teams led by underserved founders for free.
+                a voice and a platform to diverse peoples. Consequently, I won't knowingly work on projects
+                that cause harm to marginalized communities or persist existing inequalities, nor with project
+                founders who do not agree with those goals. I'm learning all the time.
             </p>
         </div>
     </body>
