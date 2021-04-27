2b7182ec115fc4e52418c9870583f5b0727e11e7
Ben Werdmuller <benwerdmuller@Werd.local>
2021-04-26 17:52:18 -0700

    First version

--- index.php
+++ index.php
@@ -18,13 +18,16 @@
         <meta charset="utf-8">
 
         <!-- I hate writing descriptions of myself ... if you can think of a better one, hit me up: ben@benwerd.com -->
-        <meta name="description" content="Ben Werdmüller is an engineer, founder, investor, and writer. He works on ethical tech.">
+        <meta name="description" content="Ben Werdmüller is an engineer, founder, investor, and writer.">
 
         <!-- I wrote this HTML code with my own two fingers -->
         <meta name="generator" content="Handmade">
 
         <!-- Unfortunately, I wrote this CSS, too -->
         <link rel="stylesheet" href="/css/main.css">
+
+        <!-- Nothing says "I love the open web" like linking to a proprietary font, right? -->
+        <link rel="stylesheet" href="https://use.typekit.net/ffz6nuq.css">
 
         <!-- Credit for this emoji favicon hack goes to Lea Verou: https://twitter.com/leaverou/status/1241619866475474946?lang=en -->
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎉</text></svg>">
@@ -55,8 +58,61 @@
 
     </head>
     <body>
+        <div class="ben">
+            <img src="/images/benpoints.png">
+        </div>
         <h1>
-            Hello, I'm Ben Werdmüller
+            Hello, I'm Ben Werdmüller.
         </h1>
+        <h2>
+            I'm an engineer, startup founder, investor, and writer.
+        </h2>
+        <div class="desc">
+            <p>
+                I
+                <a href="https://werd.io" target="_blank">blog at werd.io</a>,
+                <a href="https://twitter.com/benwerd" target="_blank" rel="me noopener">tweet at @benwerd</a>,
+                and
+                <a href="https://www.linkedin.com/in/benwerd/" target="_blank" rel="noopener">try to do work
+                that makes the world more equal and informed</a>.
+            </p>
+            <p>
+                If you like, you can
+                <a href="https://subscribe.werd.io" target="_blank" rel="noopener">subscribe to my newsletter</a>.
+            </p>
+        </div>
+        <div class="but-wait-theres-more">
+            <h3>
+                I'm trying to use the web for good.
+            </h3>
+            <p>
+                I like to work on projects with the potential to make the world more equal and empathetic.
+                Previously, that has meant building open source platforms that help people to connect and
+                learn from each other on their own terms.
+            </p>
+            <p>
+                At the start of my career, I was convinced people would use social software
+                to learn from each other, build new relationships, and work in entirely new ways.
+                I built one of the first social intranet platforms, and learned how to build a startup
+                and an open source ecosystem in the process.
+            </p>
+            <p>
+                Software I've built has been used by NGOs, Fortune 500 companies, Ivy League universities,
+                and national governments. I've invested alongside organizations like KQED and the New York Times.
+                I helped build one of the first NFT protocols for independent artists. And I've mentored hundreds
+                of startups (as well as starting a few of my own).
+            </p>
+            <p>
+                I'd love to be helpful if I can. <a href="mailto:ben@benwerd.com">Get in touch
+                at ben@benwerd.com</a>.
+            </p>
+            <p>
+                I believe in inclusivity, building real equity, and subverting systemic oppression by giving
+                a voice and a platform to diverse peoples. I will not knowingly work on projects that cause harm
+                to marginalized communities or persist existing inequalities, nor with project founders who do not
+                agree with those goals. I do not work with all-male or all-white teams, and will advise teams
+                led by underserved founders for free.
+            </p>
+        </div>
     </body>
 </html>
